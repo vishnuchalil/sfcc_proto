30000b004a899cb6e7caf8b566244a3268e15d91
Surabhi Nair <surabhi@litmus7.com>
2020-06-11 12:17:07 +0530

    gulp changes added

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,35 @@
+'use strict';
+
+var autoprefixer = require('gulp-autoprefixer');
+var csso = require('gulp-csso');
+var del = require('del');
+var gulp = require('gulp');
+var htmlmin = require('gulp-htmlmin');
+var runSequence = require('run-sequence');
+var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
+
+
 function defaultTask(cb) {
   // place code for your default task here
   cb();
 }
+
+gulp.task('sass', function() {
+	return gulp.src('cartridge/scss/*.scss') // Get source files with gulp.src
+    .pipe(sass().on('error', sass.logError)) // Sends it through a gulp plugin
+    .pipe(csso()) // Minify the file
+    .pipe(gulp.dest('cartridge/static/default/css')) // Outputs the file in the destination folder
+
+});
+
+//Gulp task to minify JavaScript files
+gulp.task('scripts', function() {
+  return gulp.src('cartridge/js/*.js')
+    // Minify the file
+    .pipe(uglify())
+    // Output
+    .pipe(gulp.dest('cartridge/static/default/js'))
+});
 
 exports.default = defaultTask
